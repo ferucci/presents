32ec3c8504e39d1709ed776458c9b1f4454aaebb
ferucci <development.first1@gmail.com>
2025-11-30 00:56:22 +0300

    moved local data to a separate folder

---
+++ src/data/documentationPage/images.ts
@@ -0,0 +1,8 @@
+export const imagesDocs = [
+  '../src/assets/images/documentation/photo_23_2025-11-29_22-39-27.jpg',
+  '../src/assets/images/documentation/photo_1_2025-11-29_22-39-27.jpg',
+  '../src/assets/images/documentation/photo_2_2025-11-29_22-39-27.jpg',
+  '../src/assets/images/documentation/photo_3_2025-11-29_22-39-27.jpg',
+  '../src/assets/images/documentation/photo_4_2025-11-29_22-39-27.jpg',
+  '../src/assets/images/documentation/photo_24_2025-11-29_22-39-27.jpg'
+];
