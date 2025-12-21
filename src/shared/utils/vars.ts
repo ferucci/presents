ff4641db7f0a2193aea7462fd2a7e758731ff992
ferucci <development.first1@gmail.com>
2025-12-21 22:04:33 +0300

    delete comment

--- src/shared/utils/vars.ts
+++ src/shared/utils/vars.ts
@@ -1,3 +1,2 @@
-// Получаем имя бота из переменных окружения или используем значение по умолчанию
 export const TELEGRAM_BOT_NAME = process.env.NEXT_PUBLIC_TELEGRAM_BOT;
 export const TELEGRAM_URL = `https://t.me/${TELEGRAM_BOT_NAME}`;
